7962cee18c06f84c9030d390ed416cd4a90cd7f9
OMG-maker <issac_oh@naver.com>
2025-06-20 16:38:32 +0900

    Docs: ReviewRepository 미사용 메서드 주석처리
    - ReviewRepositoryCustom 에서 선언하고 이후 구현한 메서드를 사용중이므로 제거

--- src/main/java/com/example/bookmarket/review/repository/ReviewRepository.java
+++ src/main/java/com/example/bookmarket/review/repository/ReviewRepository.java
@@ -1,14 +1,10 @@
 package com.example.bookmarket.review.repository;
 
-import com.example.bookmarket.book.entity.Book;
 import com.example.bookmarket.review.entity.Review;
-import com.example.bookmarket.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRepositoryCustom {
-    Optional<Review> findByBookAndUser(Book book, User user);
+//    Optional<Review> findByBookAndUser(Book book, User user);
 }
